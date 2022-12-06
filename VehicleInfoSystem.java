9005eec41320e14e41cc5057caf6636b0120c30d
Sathira <45524280+pwSathira@users.noreply.github.com>
2022-12-06 18:54:34 -0500

    Update VehicleInfoSystem.java

--- VehicleInfoSystem.java
+++ VehicleInfoSystem.java
@@ -3,72 +3,47 @@ import java.util.Scanner;
 
 public class VehicleInfoSystem {
     public static void main(String args[]){
-        //Key: Time in MS
-        //Value: Speed at that time
-        HashMap<String, String> M = new HashMap<String, String>();
+        HashMap<String, String> M = new HashMap<String, String>();// create a HashMap to store key-value pairs
         Scanner sc = new Scanner(System.in);
         boolean complete = false;
 
         while(!complete){
             System.out.print("(HashMap) Enter (i)nsert, (d)elete, (s)earch, (p)rint or (q)uit: ");
-            String userIn = sc.next().toLowerCase();
-            if(userIn.charAt(0) == 'i'){
+            String userIn = sc.next().toLowerCase();  // convert user input to lower case
+            if(userIn.charAt(0) == 'i'){ // if user entered "i", insert a new key-value pair into the HashMap
                 System.out.print("Enter potential obstacle: ");
                 String key = sc.next();
                 System.out.print("Enter the distance from vehicle (in km): ");
                 String value = sc.next();
-                M = addKey(key, value, M);
-            } else if (userIn.charAt(0) == 'd') {
+                M = addKey(key, value, M);// call the addKey() method to insert the key-value pair
+            } else if (userIn.charAt(0) == 'd') {  // if user entered "d", delete a key-value pair from the HashMap
                 System.out.print("Enter the delete term: ");
                 String key = sc.next();
-                M = removeKey(key, M);
+                M = removeKey(key, M);// call the removeKey() method to delete the key-value pair
             } else if (userIn.charAt(0) == 's') {
-                System.out.print("Enter the search key: ");
+                System.out.print("Enter the search key: "); // if user entered "s", search the HashMap for a given key
                 String key = sc.next();
-                System.out.println("The result is : " + M.get(key));
-            } else if (userIn.charAt(0) == 'p') {
+                System.out.println("The result is : " + M.get(key)); // use the get() method to search for the given key
+            } else if (userIn.charAt(0) == 'p') {  // if user entered "p", print all key-value pairs in the HashMap
             	for (String key : M.keySet()){
                     String value = M.get(key);
                     System.out.println(key + " is " + value + "km away.");
                 }
-            } else if (userIn.charAt(0) == 'q') {
+            } else if (userIn.charAt(0) == 'q') {// if user entered "q", quit the program
                 complete = true;
             } else {
-                System.out.println("Input not recognized try again!");
+                System.out.println("Input not recognized try again!"); // if user input is not recognized, prompt them to try again
             }
         }
         sc.close();
     }
-    public static HashMap<String, String> getUserInput(HashMap<String, String> m) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Number of inputs: ");
-        int numOfIn = sc.nextInt();
-
-        for (int i = 0; i < numOfIn; i++){
-            System.out.print("Enter Key: ");
-            String userKey = sc.next();
-            System.out.print("Enter Value: ");
-            String userVal = sc.next();
-            m = addKey(userKey, userVal, m);
-        }
-        System.out.print("Enter search key: ");
-        String searchKey = sc.next();
-        String result = searchKey(searchKey, m);
-        System.out.print("Result is: " + result + "\n");
-
-        System.out.print("Delete key: ");
-        String deleteKey = sc.next();
-        m = removeKey(deleteKey, m);
-
-        System.out.print("Insert Key: ");
-        String insKey = sc.next();
-        System.out.print("Insert Value: ");
-        String insVal = sc.next();
-        m = addKey(insKey, insVal, m);
-
-        sc.close();
-        return m;
-    }
+   
+    /*
+     * This method searches the given HashMap for the given key
+     * key - the key to search for
+     * m - the HashMap to search
+     * returns - the value associated with the given key, or "search term does not exist!" if the key is not found
+     */
     public static String searchKey(String key, HashMap<String, String> m) {
         String value = "";
         value = m.get(key);
@@ -77,10 +52,25 @@ public class VehicleInfoSystem {
         }
         return value;
     }
+    /*
+     * This method removes the given key-value pair into the given HashMap
+     * k - the key to delete
+     * m - the HashMap to insert into
+     * returns - the updated HashMap
+     */
     public static HashMap<String, String> removeKey(String key, HashMap<String, String> m) {
+    	// remove the key-value pair with the given key from the given HashMap
         m.remove(key);
         return m;
     }
+    
+    /*
+     * This method inserts the given key-value pair into the given HashMap
+     * k - the key to insert
+     * v - the value to insert
+     * m - the HashMap to insert into
+     * returns - the updated HashMap
+     */
     public static HashMap<String, String> addKey(String k, String v, HashMap<String,String> m) {
         m.put(k,v);
         return m;
